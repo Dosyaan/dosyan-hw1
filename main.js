e663e3b970907cccb81f2670a9e1390a5d522b85
Dosyaan <dastanmatraimov79@gmail.com>
2024-10-12 18:37:40 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+// task - 4
+
+// const input = document.getElementById('inputField');
+// const show = document.getElementById('show');
+
+// console.log(show);
+// show.addEventListener('click', function() {
+//     inputField.value = 'Hello, world!';
+// })
+
+// task - 7
+
+// var clickCount = 0;
+// const countButton = document.getElementById("countButton");
+// const count = document.getElementById('count');
+// console.log(countButton);
+
+//     countButton.addEventListener("click", function() {
+//     clickCount++;
+//     count.textContent = "Кликов сделано: " + clickCount;
+// })
+
+
+// task - 9
+
+// const input = document.getElementById('inputField');
+// const check = document.getElementById('check');
+// const error = document.getElementById('error');
+
+// check.addEventListener('click', function() {
+
+//     if (inputField.value.trim () === '') {
+//         error.style.display = "block"
+//     } 
+//     else {
+//         error.textContent = "";
+//         alert("Поле заполнено!");
+//     }
+// })
+
+
+// task - 10
+
+// const styleButton = document.getElementById("changeButton");
+// const text = document.getElementById("text");
+
+// styleButton.addEventListener("click", function() {
+//     text.style.fontWeight = "bold";
+//     text.style.fontFamily = "Times New Roman"
+//     text.style.fontSize = "24px";
+// })
